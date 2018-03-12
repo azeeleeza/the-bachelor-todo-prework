d2024d4252a3b4f0fa9682f0e343f0f671d4a9e2
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-12 02:06:25 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,19 +3,18 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-    found=data.keys.each do |season|
+  data.keys.each do |season|
       data[season].each do |people|
         # people.find do |k,v|
         #   v = occupation
         #
         # end
          if people["occupation"] == occupation
-           return people["name"]
+            people["name"]
          end
       end
     end
 
-    puts found
 end
 
 def count_contestants_by_hometown(data, hometown)
