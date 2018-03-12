3f9f857fdaf9d6fae6f2cd0f960b92637b4ce338
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-12 01:44:23 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,7 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  puts data[season].find do |element|
+    element.values.include?("Winner")
+  end
 end
 
 def get_contestant_name(data, occupation)
