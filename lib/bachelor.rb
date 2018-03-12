b2e4120a766a3111d2b5ee31d15c4556f4c35b6d
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-12 02:18:11 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,48 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find { |element| element.values.include?("Winner")}["name"].split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.keys.each do |season|
+      data[season].each do |person|
+         if person["occupation"] == occupation
+            return person["name"]
+         end
+      end
+    end
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count=0
+  data.keys.each do |season|
+      data[season].each do |person|
+         if person["hometown"] == hometown
+            count+= 1
+         end
+      end
+    end
+    count
 end
 
 def get_occupation(data, hometown)
-  # code here
-end
+  data.keys.each do |season|
+      data[season].each do |person|
+         if person["hometown"] == hometown
+            return person["occupation"]
+         end
+      end
+    end
+
+  end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total=0
+  count=0
+  data[season].each do |person|
+    total+=person["age"].to_i
+    count+=1
+  end
+avg = total/count.to_f
+avg.round(0)
 end
